ab46bb5c8a410867bf914f739ed5d61a46ea844f
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-08-12 10:12:03 +0800

    Using image dir

--- matlab/colorHistogramDemo.m
+++ matlab/colorHistogramDemo.m
@@ -5,13 +5,15 @@
 clc
 clear all
 
+image_dir = fullfile(fileparts(pwd), 'images');
+
 images = { ... %'peppers.png', 'saturn.png', 'pears.png', ...
            ... %'coloredChips.png', 'gantrycrane.png', ...
            'bag1.jpg', 'shoe1.jpg', 'shoe2.jpg', ...
          };
 for str = images
     name = char(str);
-    image = load_image(name);
+    image = load_image(fullfile(image_dir, name));
     colorHistogram(image);
     %print(sprintf('%s-histogram.eps', name), '-depsc2', '-f1')
     %print(sprintf('%s-histogram.png', name), '-dpng', '-f1')
--- matlab/hogDemo.m
+++ matlab/hogDemo.m
@@ -5,13 +5,15 @@
 clc
 clear all
 
+image_dir = fullfile(fileparts(pwd), 'images');
+
 images = { ... %'peppers.png', 'saturn.png', 'pears.png', ...
            ... %'coloredChips.png', 'gantrycrane.png', ...
            'bag1.jpg', 'shoe1.jpg', 'shoe2.jpg', ...
          };
 for str = images
     name = char(str);
-    image = load_image(name);
+    image = load_image(fullfile(image_dir, name));
     hogDescriptors(image, 'cellsize', 8, 'bins', 8);
     %print(sprintf('%s-hog.eps', name), '-depsc2', '-f1')
     %print(sprintf('%s-hog.png', name), '-dpng', '-f1')
