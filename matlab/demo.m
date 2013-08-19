16ed14ee4c3fa1ff0573692d6189ff91fe07b83f
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-08-19 16:23:37 +0800

    Fix demo script

--- matlab/demo.m
+++ matlab/demo.m
@@ -7,10 +7,11 @@ environment()
 global DATA_DIR
 
 force_hist = false;
-force_index = true;
+force_index = false;
+num_query = 25;
 
-%category = 'bag';
-category = 'shoe'; 
+category = 'bag';
+% category = 'shoe'; 
 
 vocabulary = buildVocabulary(category, 'force', force_hist);
 [histograms, names] = buildHistograms(category, vocabulary, 'force', force_hist);
@@ -28,21 +29,22 @@ combined = [hog; colors];
 combined_index = build_index(category, combined, 'combined', 'force', force_index);
 
 % query image
-%image = imread(fullfile(DATA_DIR, 'test', 'T11oxTBsDT1RCvBVdK.jpg'));
-image = imread(fullfile(DATA_DIR, 'test', '6jw0_kqytgwkfm5bgu2dwgfjeg5sckzsew_1211x1032.jpg_468x468.jpg.jpg'));
-%image = imread(fullfile(DATA_DIR, 'test', 'T1kXaoXdFBXXc2GD.8_072102.jpg_310x310.jpg'));
+images = dir(fullfile(DATA_DIR, 'test', [category '*.jpg']));
+images = {images.name}';
+image = imread(fullfile(DATA_DIR, 'test', images{1}));
+
 query = computeHistogramFromImage(vocabulary, image);
 figure(4), imshow(image)
 
 % query by shape
-[indices,rank] = query_index(hog_index, hog, query.hog');
+[indices,rank] = query_index(hog_index, hog, query.hog', num_query);
 figure(1), display_matches(image, names, indices, rank);
 
 % query by color
-[indices,rank] = query_index(colors_index, colors, query.colors');
+[indices,rank] = query_index(colors_index, colors, query.colors', num_query);
 figure(2), display_matches(image, names, indices, rank);
 
 % query by shape and color
-[indices,rank] = query_index(combined_index, combined, [query.hog, query.colors]');
+[indices,rank] = query_index(combined_index, combined, [query.hog, query.colors]', num_query);
 figure(3), display_matches(image, names, indices, rank);
 
--- matlab/display_matches.m
+++ matlab/display_matches.m
@@ -5,7 +5,7 @@ multi = true;
 matches = filenames(indexes(indexes~=0)); % discard any 0
 
 if multi
-    imdisp(matches)
+    imdisp(matches, 'size', 5);
 else
     subplot(121), subimage(image), axis off, title('query image');
     for j = 1:length(matches);
