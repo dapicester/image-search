8fd0d8db3a21416ed898f9494d81fd5b932c151a
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-08-20 11:13:24 +0800

    Add parfor option to setup

--- matlab/setup.m
+++ matlab/setup.m
@@ -9,20 +9,28 @@ if nargin < 1, what = 'all'; end
 
 root = fileparts(pwd);
 
-switch what
-    case { 'vlfeat', 'all' }
-        if exist('vl_version', 'file') ~= 3
-            setup_vlfeat
-        end
+if any(strcmp(what, { 'vlfeat', 'all' }))
+    setup_vlfeat
+end
+if any(strcmp(what, { 'parfor', 'all' }))
+    setup_parfor
 end
 
 function setup_vlfeat()
-    vlfeat_path = fullfile(root, 'dependencies', 'vlfeat-0.9.16');
-    assert(exist(vlfeat_path, 'dir') == 7, 'VLFeat path not found: %s', vlfeat_path)
-    
-    script = fullfile(vlfeat_path, 'toolbox', 'vl_setup');
-    run(script)
-    assert(exist('vl_version', 'file') == 3, 'Failed to setup VLFeat')
+    if exist('vl_version', 'file') ~= 3
+        vlfeat_path = fullfile(root, 'dependencies', 'vlfeat-0.9.16');
+        assert(exist(vlfeat_path, 'dir') == 7, 'VLFeat path not found: %s', vlfeat_path)
+
+        script = fullfile(vlfeat_path, 'toolbox', 'vl_setup');
+        run(script)
+        assert(exist('vl_version', 'file') == 3, 'Failed to setup VLFeat')
+    end
+end
+
+function setup_parfor()
+    if exist('matlabpool', 'file') == 2 && matlabpool('size') == 0
+        matlabpool('open')
+    end
 end
 
 end
