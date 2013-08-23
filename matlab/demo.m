23632189d36d1f2cf66c9f3386c8dbb946205b46
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-08-23 10:27:32 +0800

    Re-enabled combined search

--- matlab/demo.m
+++ matlab/demo.m
@@ -35,31 +35,32 @@ hsv = cat(1, histograms.hsvcolors)';
 hsv_index = build_index(category, hsv, 'hsv', 'force', force_index);
 
 % both
-% combined = [hog; colors];
-% combined_index = build_index(category, combined, 'combined', 'force', force_index);
+combined = [hog; colors];
+combined_index = build_index(category, combined, 'combined', 'force', force_index);
 
 % query image
 images = dir(fullfile(DATA_DIR, 'test', [category '*.jpg']));
 images = {images.name}';
-image = imread(fullfile(DATA_DIR, 'test', images{5}));
+image = imread(fullfile(DATA_DIR, 'test', images{1}));
 
 query = computeHistogramFromImage(vocabulary, image);
 figure(1), imshow(image), title('Query image')
 
 % by shape
-[indices,rank] = query_index(hog_index, hog, query.hog', num_query);
-figure(2), display_matches(image, names, indices, rank);
+[shape_indices,shape_rank] = query_index(hog_index, hog, query.hog', num_query);
+figure(2), display_matches(image, names, shape_indices, shape_rank);
 
 % by color
-[indices,rank] = query_index(colors_index, colors, query.colors', num_query);
-figure(3), display_matches(image, names, indices, rank);
+[color_indices,color_rank] = query_index(colors_index, colors, query.colors', num_query);
+figure(3), display_matches(image, names, color_indices, color_rank);
 
 % by hsv-color
-[indices,rank] = query_index(hsv_index, hsv, query.hsvcolors', num_query);
-figure(4), display_matches(image, names, indices, rank);
-
+[hsv_indices,hsv_rank] = query_index(hsv_index, hsv, query.hsvcolors', num_query);
+figure(4), display_matches(image, names, hsv_indices, hsv_rank);
 
 % query by shape and color
-% [indices,rank] = query_index(combined_index, combined, [query.hog, query.colors]', num_query);
-% figure(4), display_matches(image, names, indices, rank);
+[combined_indices,combined_rank] = query_index(combined_index, combined, [query.hog query.colors]', num_query);
+figure(5), display_matches(image, names, combined_indices, combined_rank);
 
+% cleanup
+clear force_vocab force_hist force_index num_query 
