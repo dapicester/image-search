714558ea05d5556a69ae9d4ce3b821b90ad7fc01
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-09-06 12:39:27 +0800

    Added tests

---
+++ libvis/test/opencv_sanity.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file opencv_sanity.cpp
+ * @brief Sanity test for the OpenCV libraries
+ * @author Paolo D'Apice
+ */
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        cout << "No image" << endl;
+        return 1;
+    }
+
+    Mat image = imread(argv[1]);
+    if (!image.data) {
+        cout << "No image data" << endl;
+        return 2;
+    }
+
+    imshow("Display image", image);
+
+    if (argc > 2) {
+        cout << "Press a key to continue" << endl;
+        waitKey(0);
+    }
+
+    return 0;
+}
