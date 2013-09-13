622e179c31417695c5a70bbc01a316149320019c
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-09-13 08:55:53 +0800

    Refactoring

--- libvis/test/test_kmeans.cpp
+++ libvis/test/test_kmeans.cpp
@@ -8,26 +8,12 @@
 #include <boost/test/unit_test.hpp>
 
 #include "kmeans.hpp"
+#include "test_utils.hpp"
 
 using namespace cv;
 
-Mat getTestData(int dimension, int numData) {
-    VlRand rand;
-    vl_rand_init(&rand);
-    vl_rand_seed(&rand, 1000);
-
-    Mat data(dimension, numData, CV_32F);
-    for(int i = 0; i < dimension; i++) {
-        for(int j = 0; j < numData; j++) {
-            float randomNum = (float) vl_rand_real3(&rand) + 1;
-            data.at<float>(i,j) = randomNum;
-        }
-    }
-    return data;
-}
-
 BOOST_AUTO_TEST_CASE(clusters) {
-    Mat data = getTestData(256, 10000);
+    Mat data = getTestData<float>(256, 10000);
     VlKMeans* centers = vis::kmeans(data, 40);
     vl_kmeans_delete(centers);
 }
