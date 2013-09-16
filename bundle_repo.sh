072b0097c3547331864f60de51f72e473cdd5594
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-09-16 16:31:03 +0800

    Verbose bundle_repo.sh

--- bundle_repo.sh
+++ bundle_repo.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-git bundle create image-search-`date +%Y%m%d`.git --all --tags --remotes
+file="image-search-`date +%Y%m%d`.git"
+git bundle create $file --all --tags --remotes
+echo "Created bundle $file"
+
