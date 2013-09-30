b2703d9ee36069778eb655e567d0f04655f7f34e
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-09-30 16:44:39 +0800

    Removed namespace alias from header file

--- libvis/include/utils/filesystem.hpp
+++ libvis/include/utils/filesystem.hpp
@@ -15,23 +15,21 @@
 
 namespace vis {
 
-namespace fs = boost::filesystem;
-
 /// Match image file paths.
 struct has_image_extension {
-    bool operator()(const fs::path& p) {
-        const fs::path ext = p.extension();
+    bool operator()(const boost::filesystem::path& p) {
+        const boost::filesystem::path ext = p.extension();
         return ext == ".png" || ext == ".jpg";
     }
 };
 
 /// Get image files in given directory.
-std::vector<fs::path>
-getImageFiles(const fs::path& dir) {
-    std::vector<fs::path> files;
+std::vector<boost::filesystem::path>
+getImageFiles(const boost::filesystem::path& dir) {
+    std::vector<boost::filesystem::path> files;
 
-    fs::directory_iterator it = fs::directory_iterator(dir),
-                           end = fs::directory_iterator();
+    boost::filesystem::directory_iterator it = boost::filesystem::directory_iterator(dir),
+                           end = boost::filesystem::directory_iterator();
 
     std::copy(boost::make_filter_iterator<has_image_extension>(it, end),
               boost::make_filter_iterator<has_image_extension>(end, end),
@@ -41,9 +39,9 @@ getImageFiles(const fs::path& dir) {
 }
 
 // TODO unit test for this
-void path2string(const std::vector<fs::path>& paths, std::vector<std::string>& names) {
+void path2string(const std::vector<boost::filesystem::path>& paths, std::vector<std::string>& names) {
     std::transform(paths.begin(), paths.end(), std::back_inserter(names),
-                   std::mem_fun_ref<std::string,fs::path>(&fs::path::string));
+                   std::mem_fun_ref<std::string,boost::filesystem::path>(&boost::filesystem::path::string));
 }
 
 } /* namespace vis */
