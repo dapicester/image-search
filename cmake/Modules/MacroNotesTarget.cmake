08b773c9d128dff6ccc14e81efac59346d74af36
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-10-09 16:17:59 +0800

    Enhanced notes target using ack-grep

--- cmake/Modules/MacroNotesTarget.cmake
+++ cmake/Modules/MacroNotesTarget.cmake
@@ -2,8 +2,14 @@
 # Usage:
 #   add_notes_target()
 macro(add_notes_target)
+    find_program(ACK NAMES ack ack-grep)
+    if (ACK-NOTFOUND)
+        set(command "grep -inRE 'todo|fixme|xxx' *")
+    else()
+        set(command "ack -i '(todo|fixme|xxx):?[[:space:]]+(.*)' --output=' $1 $2'")
+    endif()
     add_custom_target(notes
-        COMMAND find . | xargs grep --color=always -iRE "todo|fixme|xxx" | sort | uniq
+        COMMAND sh -c ${command} && echo
         VERBATIM
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         COMMENT "Showing notes")
