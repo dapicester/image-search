b0e6db29eb80dc02cc9eb57eb2ac1e83353fd955
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-10-09 12:14:57 +0800

    Moved implementation to separate header

---
+++ libvis/include/detail/extract_impl.hpp
@@ -0,0 +1,80 @@
+/**
+ * @file detail/extract.hpp
+ * @brief Descriptors and histograms computation
+ * @author Paolo D'Apice
+ */
+
+#ifndef VIS_DETAIL_EXTRACT_IMPL_HPP
+#define VIS_DETAIL_EXTRACT_IMPL_HPP
+
+#include "standardize.hpp"
+#include "utils.hpp"
+#include <cstdio>
+
+namespace vis {
+
+template <typename Callback>
+void
+extract(const std::vector<boost::filesystem::path>& names, cv::Mat& output, const Callback& cb) {
+    cv::Mat descriptors;
+
+    // TODO openmp parfor
+    size_t numImages = names.size();
+    size_t i = 0;
+    std::vector<boost::filesystem::path>::const_iterator it = names.begin(), end = names.end();
+
+    for (; it != end; ++it) {
+        const std::string& name = it->string();
+        printf("  Extracting features from %s (%lu/%lu)\n", name.c_str(), i+1, numImages);
+
+        cv::Mat input = cv::imread(name, CV_LOAD_IMAGE_GRAYSCALE); // FIXME only hog needs grayscale
+        cv::Mat image; // TODO change standardize signature to allow return?
+        standardizeImage(input, image);
+
+        cv::Mat m = cb(image);
+
+        descriptors.push_back(m);
+        ++i;
+    }
+    output = descriptors.t();
+}
+
+HogVocabularyCallback::HogVocabularyCallback(size_t n)
+        : numFeatures(n) {}
+
+cv::Mat
+HogVocabularyCallback::operator()(const cv::Mat& image) const {
+    cv::Mat d = hog.extract(image).toMat();
+    d = colsubset<float>(d, numFeatures, UNIFORM);
+    d = d.t();
+    return d;
+}
+
+BagOfWords::BagOfWords(const Vocabulary* v)
+        : vocabulary(v) {}
+
+cv::Mat
+BagOfWords::operator()(const cv::Mat& descriptors) const {
+    cv::Mat words = vocabulary->quantize(descriptors);
+    cv::Mat histogram = hist(words, vocabulary->getNumWords(), SUM1);
+    return histogram;
+}
+
+HogBagOfWordsCallback::HogBagOfWordsCallback(const Vocabulary* v)
+        : bow(v) {}
+
+cv::Mat
+HogBagOfWordsCallback::operator()(const cv::Mat& image) const {
+    cv::Mat d = hog.extract(image).toMat();
+    cv::Mat hist = bow(d).t();
+    return hist;
+}
+
+
+
+
+
+} /* namespace vis */
+
+#endif /* VIS_DETAIL_EXTRACT_IMPL_HPP */
+
--- libvis/include/extract.hpp
+++ libvis/include/extract.hpp
@@ -1,13 +1,16 @@
+/**
+ * @file extract.hpp
+ * @brief Descriptors and histograms computation
+ * @author Paolo D'Apice
+ */
+
 #ifndef VIS_EXTRACT_HPP
 #define VIS_EXTRACT_HPP
 
 #include "hog.hpp"
-#include "standardize.hpp"
 #include "vocabulary.hpp"
-#include "utils.hpp"
 #include <boost/filesystem/path.hpp>
 #include <vector>
-#include <cstdio>
 
 namespace vis {
 
@@ -21,40 +24,12 @@ namespace vis {
  */
 template <typename Callback>
 void
-extract(const std::vector<boost::filesystem::path>& names, cv::Mat& output, const Callback& cb) {
-    cv::Mat descriptors;
-
-    // TODO openmp parfor
-    size_t numImages = names.size();
-    size_t i = 0;
-    std::vector<boost::filesystem::path>::const_iterator it = names.begin(), end = names.end();
-
-    for (; it != end; ++it) {
-        const std::string& name = it->string();
-        printf("  Extracting features from %s (%lu/%lu)\n", name.c_str(), i+1, numImages);
-
-        cv::Mat input = cv::imread(name, CV_LOAD_IMAGE_GRAYSCALE); // FIXME only hog needs grayscale
-        cv::Mat image; // TODO change standardize signature to allow return?
-        standardizeImage(input, image);
-
-        cv::Mat m = cb(image);
-
-        descriptors.push_back(m);
-        ++i;
-    }
-    output = descriptors.t();
-}
+extract(const std::vector<boost::filesystem::path>& names, cv::Mat& output, const Callback& cb);
 
 /// Compute HOGs for constructing a word vocabulary.
 struct HogVocabularyCallback {
-    HogVocabularyCallback(size_t n) : numFeatures(n) {}
-
-    cv::Mat operator()(const cv::Mat& image) const {
-        cv::Mat d = hog.extract(image).toMat();
-        d = colsubset<float>(d, numFeatures, UNIFORM);
-        d = d.t();
-        return d;
-    }
+    HogVocabularyCallback(size_t numFeatures);
+    cv::Mat operator()(const cv::Mat& image) const;
 private:
     size_t numFeatures;
     HogExtractor hog;
@@ -62,33 +37,24 @@ private:
 
 /// Compute bag-of-words using the given vocabulary.
 struct BagOfWords {
-    BagOfWords(const Vocabulary* v) : vocabulary(v), numWords(v->getNumWords()) {}
-
-    cv::Mat operator()(const cv::Mat& descriptors) const {
-        cv::Mat words = vocabulary->quantize(descriptors);
-        cv::Mat histogram = hist(words, numWords, SUM1);
-        return histogram;
-    }
+    BagOfWords(const Vocabulary* vocabulary);
+    cv::Mat operator()(const cv::Mat& descriptors) const;
 private:
     const Vocabulary* vocabulary;
-    size_t numWords;
 };
 
 /// Compute HOG bag-of-words.
 struct HogBagOfWordsCallback {
-    HogBagOfWordsCallback(const Vocabulary* v) : bow(v) {}
-
-    cv::Mat operator()(const cv::Mat& image) const {
-        cv::Mat d = hog.extract(image).toMat();
-        cv::Mat hist = bow(d).t();
-        return hist;
-    }
+    HogBagOfWordsCallback(const Vocabulary* v);
+    cv::Mat operator()(const cv::Mat& image) const;
 private:
     BagOfWords bow;
     HogExtractor hog;
 };
 
 } /* namespace vis */
+
+#include "detail/extract_impl.hpp"
 
 #endif /* VIS_EXTRACT_HPP */
 
