d807d1e1f2379a77b02dc3bbfb06776028d2aecf
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-10-23 13:47:16 +0800

    Refactoring gui utils

--- demo/src/utils.cpp
+++ demo/src/utils.cpp
@@ -13,6 +13,9 @@ namespace fs = boost::filesystem;
 using std::string;
 using std::vector;
 
+static const QString DATA_EXT = ".dgz";
+static const QString TEXT_EXT = ".txt";
+
 vector<string>
 loadNames(const fs::path& file) {
     vector<string> names;
@@ -34,19 +37,38 @@ loadNames(const fs::path& file, const fs::path& prefix) {
 }
 
 fs::path
+categoryFile(const fs::path& dataDir, const QString& category) {
+    QString file(category);
+    file.append(TEXT_EXT);
+    return dataDir / file.toStdString();
+}
+
+fs::path
+categoryDir(const fs::path& dataDir, const QString& category) {
+    return dataDir / category.toStdString();
+}
+
+fs::path
 vocabularyFile(const fs::path& dataDir, const QString& category) {
-    QString file = category;
-    file.prepend("vocabulary_");
-    file.append(".dgz");
+    QString file = "vocabulary_";
+    file.append(category);
+    file.append(DATA_EXT);
+    return dataDir / file.toStdString();
+}
+
+fs::path
+descriptorsFile(const fs::path& dataDir, const QString& category, const QString& type) {
+    QString file = "descriptors_";
+    file.append(type).append("_").append(category);
+    file.append(DATA_EXT);
     return dataDir / file.toStdString();
 }
 
 fs::path
 indexFile(const fs::path& dataDir, const QString& category, const QString& type) {
-    QString file = category;
-    file.append("_");
-    file.append(type);
-    file.append("_index.dgz");
+    QString file = "index_";
+    file.append(type).append("_").append(category);
+    file.append(DATA_EXT);
     return dataDir / file.toStdString();
 }
 
--- demo/src/utils.hpp
+++ demo/src/utils.hpp
@@ -19,11 +19,28 @@ loadNames(const boost::filesystem::path& file);
 std::vector<boost::filesystem::path>
 loadNames(const boost::filesystem::path& file, const boost::filesystem::path& prefix);
 
+/// Get the path to the text file containing images for the given category.
+/// @return path to \c dataDir/category.txt
+boost::filesystem::path
+categoryFile(const boost::filesystem::path& dataDir, const QString& category);
+
+/// Get the path to the directory containing images for the given category.
+/// @return path to \c dataDir/category/
+boost::filesystem::path
+categoryDir(const boost::filesystem::path& dataDir, const QString& category);
+
 /// Get the path to the vocabulary file for the given category.
+/// @return path to \c dataDir/vocabulary_category.dgz
 boost::filesystem::path
 vocabularyFile(const boost::filesystem::path& dataDir, const QString& category);
 
+/// Get the path to the descriptors file for the given category.
+/// @return path to \c dataDir/descriptors_type_category.dgz
+boost::filesystem::path
+descriptorsFile(const boost::filesystem::path& dataDir, const QString& category, const QString& type);
+
 /// Get the path to the index file for the given category.
+/// @return path to \c dataDir/index_type_category.dgz
 boost::filesystem::path
 indexFile(const boost::filesystem::path& dataDir, const QString& category, const QString& type);
 
