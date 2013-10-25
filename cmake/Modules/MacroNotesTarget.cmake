2959c41752559c2184ca3baf91ef9608bdb538c0
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-10-25 10:47:35 +0800

    CMake macros for notes support multiple projects

--- cmake/Modules/MacroNotesTarget.cmake
+++ cmake/Modules/MacroNotesTarget.cmake
@@ -4,13 +4,30 @@
 macro(add_notes_target)
     find_program(ACK NAMES ack ack-grep)
     if (ACK-NOTFOUND)
-        set(command "grep -inRE 'todo|fixme|xxx' *")
+        set(command "grep -inRE 'todo|fixme|xxx' ${CMAKE_CURRENT_SOURCE_DIR}/*")
     else()
-        set(command "ack -i '(todo|fixme|xxx):?[[:space:]]+(.*)' --output=' $1 $2'")
+        set(command "${ACK} -i '(todo|fixme|xxx):?[[:space:]]+(.*)' --nopager --output=' $1 $2' ${CMAKE_CURRENT_SOURCE_DIR}")
     endif()
-    add_custom_target(notes
+    message(STATUS "Will generate notes for project ${PROJECT_NAME}")
+    add_custom_target(${PROJECT_NAME}_notes
         COMMAND sh -c ${command} && echo
         VERBATIM
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         COMMENT "Showing notes")
 endmacro()
+
+# Shortcut for calling the 'notes' targets for all the given projects.
+# Usage:
+#   - in ProjectA/CMakeLists.txt:
+#     add_notes_target()
+#   - in ProjectB/CMakeLists.txt:
+#     add_notes_target()
+#   - in CMakeLists.txt:
+#     add_notes_for(projectA projectB)
+macro(add_notes_for)
+    message(STATUS "Will generate notes for projects: ${ARGN}.")
+    foreach(arg ${ARGN})
+        list(APPEND dependencies ${arg}_notes)
+    endforeach()
+    add_custom_target(notes DEPENDS ${dependencies})
+endmacro()
+
