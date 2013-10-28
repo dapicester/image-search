a98fcae00da7ada61ee9a71c9a4ec6eb0bd9cf90
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-10-28 12:36:42 +0800

    Added PosixTimer

---
+++ libvis/include/utils/posixtimer.hpp
@@ -0,0 +1,57 @@
+/**
+ * @file posixtimer.hpp
+ * @brief Simple timer using POSIX functions.
+ * @author Paolo D'Apice
+ */
+
+#ifndef VIS_UTILS_POSIXTIMER_HPP
+#define VIS_UTILS_POSIXTIMER_HPP
+
+#include <sys/time.h>
+
+/// @brief Type used for timestamps.
+typedef unsigned long long timestamp_t;
+
+/// @brief Simple timer using POSIX functions.
+class PosixTimer {
+public:
+    /// Start measuring time.
+    void tic() {
+        gettimeofday(&start, NULL);
+    }
+
+    /// Get measure of elapsed time since last \c tic().
+    void toc() {
+        gettimeofday(&stop, NULL);
+        elapsed = getMicroseconds(stop) - getMicroseconds(start);
+    }
+
+    /// @return Elapsed time in microseconds.
+    timestamp_t getMicros() const {
+        return elapsed;
+    }
+
+    /// @return Elapsed time in milliseconds.
+    timestamp_t getMillis() const {
+        return elapsed / 1e3;
+    }
+
+    /// @return Elapsed time in seconds.
+    timestamp_t getSeconds() const {
+        return elapsed / 1e6;
+    }
+
+private:
+
+    // Get current time in microseconds
+    timestamp_t getMicroseconds(const timeval& t) {
+        return t.tv_sec * 1000000 + t.tv_usec;
+    }
+
+    struct timeval start;
+    struct timeval stop;
+    timestamp_t elapsed;
+};
+
+#endif /* VIS_UTILS_POSIXTIMER_HPP */
+
---
+++ libvis/test/test_posixtimer.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file test_posixtimer.cpp
+ * @brief Usage test for the PosixTimer class.
+ * @author Paolo D'Apice
+ */
+
+#define BOOST_TEST_MODULE posixtimer
+#include <boost/test/unit_test.hpp>
+
+#include "utils/posixtimer.hpp"
+
+#define _CHECK_CLOSE(A,B,DELTA)
+
+BOOST_AUTO_TEST_CASE(usage) {
+    PosixTimer timer;
+    timestamp_t seconds, millis, micros;
+
+    // tic/toc
+    timer.tic();
+    sleep(1);
+    timer.toc();
+
+    seconds = timer.getSeconds();
+    BOOST_CHECK_EQUAL(seconds, 1);
+
+    millis = timer.getMillis();
+    BOOST_CHECK_EQUAL(millis, 1e3);
+
+    micros = timer.getMicros();
+    BOOST_CHECK_CLOSE(micros, 1e6, .01);
+
+    // again tic/toc
+    timer.tic();
+    sleep(1);
+    timer.toc();
+
+    seconds = timer.getSeconds();
+    BOOST_CHECK_EQUAL(seconds, 1);
+
+    millis = timer.getMillis();
+    BOOST_CHECK_EQUAL(millis, 1e3);
+
+    micros = timer.getMicros();
+    BOOST_CHECK_CLOSE(micros, 1e6, .01);
+
+    // only toc
+    sleep(1);
+    timer.toc();
+
+    seconds = timer.getSeconds();
+    BOOST_CHECK_EQUAL(seconds, 2);
+
+    millis = timer.getMillis();
+    BOOST_CHECK_EQUAL(millis, 2e3);
+
+    micros = timer.getMicros();
+    BOOST_CHECK_CLOSE(micros, 2e6, .01);
+}
+
