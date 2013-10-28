a9f3491e744760d419a4212fe7b5a14eb0e56215
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-10-28 12:36:09 +0800

    Added DISABLED flag to macro add_unit_test

--- cmake/Modules/MacroAddUnitTest.cmake
+++ cmake/Modules/MacroAddUnitTest.cmake
@@ -1,12 +1,14 @@
 # usage: add_unit_test(NAME name
 #                      SOURCES source1 source2 ...
+#                      [DISABLED true]
 #                      [LINK_LIBRARIES lib1 lib2 ...]
+#                      [WORKING_DIRECTORY dir]
 #                      [ARGS arg1 arg2 ...]
 #                      [TAG tag])
 # It makes available the variable UNIT_TEST_TARGETS containing all the unit tests
 include(MacroParseArguments)
 macro(add_unit_test)
-    parse_arguments(UNIT_TEST "NAME;SOURCES;LINK_LIBRARIES;WORKING_DIRECTORY;ARGS;TAG" ${ARGN})
+    parse_arguments(UNIT_TEST "NAME;SOURCES;DISABLED;LINK_LIBRARIES;WORKING_DIRECTORY;ARGS;TAG" ${ARGN})
     list(GET UNIT_TEST_DEFAULT_ARGS 0 UNIT_TEST_NAME)
 
     if(NOT UNIT_TEST_NAME)
@@ -17,16 +19,22 @@ macro(add_unit_test)
     if(UNIT_TEST_LINK_LIBRARIES)
         target_link_libraries(${UNIT_TEST_NAME} ${UNIT_TEST_LINK_LIBRARIES})
     endif()
+
     if(UNIT_TEST_TAG)
         set(testName "${UNIT_TEST_TAG}/${UNIT_TEST_NAME}")
     else()
         set(testName "${UNIT_TEST_NAME}")
     endif()
-    add_test(NAME ${testName}
-             WORKING_DIRECTORY ${UNIT_TEST_WORKING_DIRECTORY}
-             COMMAND ${UNIT_TEST_NAME} ${UNIT_TEST_ARGS})
-    list(APPEND UNIT_TEST_TARGETS  ${testName})
-    message(STATUS "  added test: ${testName}")
+
+    if (UNIT_TEST_DISABLED)
+        message(STATUS "  skipped test: ${testName}")
+    else()
+        add_test(NAME ${testName}
+                 WORKING_DIRECTORY ${UNIT_TEST_WORKING_DIRECTORY}
+                 COMMAND ${UNIT_TEST_NAME} ${UNIT_TEST_ARGS})
+        list(APPEND UNIT_TEST_TARGETS  ${testName})
+        message(STATUS "  added test: ${testName}")
+    endif()
 endmacro()
 
 # usage: add_all_tests_target(tests)
