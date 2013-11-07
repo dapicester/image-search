c7c04eb9bb7ad4649826cd35083d78dfe754c4f0
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-11-07 17:18:38 +0800

    Added conversion from/to Armadillo/OpenCV data types

--- libvis/test/utils/matrix.hpp
+++ libvis/test/utils/matrix.hpp
@@ -7,11 +7,13 @@
 #ifndef VIS_TEST_UTILS_MATRIX_HPP
 #define VIS_TEST_UTILS_MATRIX_HPP
 
+#include <armadillo>
 #include <opencv2/core/core.hpp>
 
 namespace test {
 
 /// @return true if matrices are equals
+static
 bool
 equals(const cv::Mat& a, const cv::Mat& b) {
     if (a.size() != b.size()) return false;
@@ -19,7 +21,17 @@ equals(const cv::Mat& a, const cv::Mat& b) {
     return cv::countNonZero(diff) == 0 ? true : false;
 }
 
+/// @return true if matrices are equals
+template <typename T>
+bool
+equals(const arma::Mat<T>& a, const arma::Mat<T>& b) {
+    if (a.n_rows != b.n_rows or a.n_cols != b.n_cols) return false;
+    arma::umat diff = (a == b);
+    return all(vectorise(diff));
+}
+
 /// @return true if matrix minimum and maximum matches the given values
+static
 bool
 hasMinMax(const cv::Mat& in, double min, double max) {
     double min_, max_;
