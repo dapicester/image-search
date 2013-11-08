978937442142cf5808e6304e687ac858f29c9ea6
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-11-08 14:35:34 +0800

    Hog using arma::fmat

--- libvis/include/hog.hpp
+++ libvis/include/hog.hpp
@@ -10,49 +10,26 @@
 extern "C" {
 #include <vl/hog.h>
 }
+#include <armadillo>
 #include <boost/noncopyable.hpp>
 #include <opencv2/core/core.hpp>
 
 namespace vis {
 
-class HogExtractor;
-
-/// @brief HOG descriptors data structure.
-struct HogDescriptors {
-
-    /// Constructor.
-    HogDescriptors(vl_size width, vl_size height, vl_size dimension);
-
-    /// Default destructor.
-    ~HogDescriptors();
-
-    /// @brief Convert to OpenCV Mat.
-    cv::Mat toMat() const;
-
-    const vl_size width;
-    const vl_size height;
-    const vl_size dimension;
-
-private:
-    float* data;
-
-    friend class HogExtractor;
-};
-
 /// @brief Extracts HOG.
 class HogExtractor : private boost::noncopyable {
 public:
     /// Constructor.
-    HogExtractor(int cellSize = 8, int numOrientations = 8); // TODO numOrientations = 9 ?
+    HogExtractor(int cellSize = 8, int numOrientations = 8 /* TODO = 9 */);
 
-    /// Default destructor.
+    /// Destructor.
     ~HogExtractor();
 
     /// @brief Extracts HOG from a gray-scale single-precision image.
-    HogDescriptors extract(const cv::Mat& image) const;
+    arma::fmat extract(const cv::Mat& image) const;
 
     /// @brief Renders HOG descriptors to a glyph image.
-    cv::Mat render(const HogDescriptors& descriptors) const;
+    cv::Mat render(const arma::fmat& descriptors) const;
 
 private:
     const int cellSize;
--- libvis/test/test_hog.cpp
+++ libvis/test/test_hog.cpp
@@ -12,36 +12,22 @@
 #include "utils/data.hpp"
 #include "utils/matrix.hpp"
 #include "utils/print.hpp"
+#include "utils/conversions.hpp"
 #include <opencv2/highgui/highgui.hpp>
-
-using cv::Mat;
-using cv::Size;
 
 #define argc boost::unit_test::framework::master_test_suite().argc
 
 BOOST_FIXTURE_TEST_CASE(compute_hog, test::Lena) {
     vis::HogExtractor extractor;
 
-    vis::HogDescriptors descriptors = extractor.extract(image);
-    BOOST_CHECK_EQUAL(descriptors.width, 16);
-    BOOST_CHECK_EQUAL(descriptors.height, 16);
-    BOOST_CHECK_EQUAL(descriptors.dimension, 3*8+4);
-
-    Mat hogMatrix = descriptors.toMat();
-    Size size = hogMatrix.size();
-    BOOST_CHECK_EQUAL(Size(16*16, 28), size); // NOTE size is (cols, rows)
-    BOOST_CHECK_EQUAL(28, hogMatrix.rows);
-    BOOST_CHECK_EQUAL(16*16, hogMatrix.cols);
-
-    // direct conversion to Mat
-    Mat hogMatrix2 = extractor.extract(image).toMat();
-    Mat diff = hogMatrix != hogMatrix2;
-    BOOST_CHECK_EQUAL(0, countNonZero(diff));
+    arma::fmat descriptors = extractor.extract(image);
+    BOOST_CHECK_EQUAL(3*8+4, descriptors.n_rows);
+    BOOST_CHECK_EQUAL(16*16, descriptors.n_cols);
 
     if (argc > 1) {
         cv::imshow("image", input);
 
-        Mat hogImage = extractor.render(descriptors);
+        cv::Mat hogImage = extractor.render(descriptors);
         cv::imshow("hog", hogImage);
 
         println("Press a key to continue");
@@ -49,28 +35,17 @@ BOOST_FIXTURE_TEST_CASE(compute_hog, test::Lena) {
     }
 }
 
-BOOST_FIXTURE_TEST_CASE(compute_hog_matrix, test::Lena) {
-    vis::HogExtractor extractor;
-
-    // descriptors and then matrix
-    vis::HogDescriptors descriptors = extractor.extract(image);
-    Mat hogMatrix = descriptors.toMat();
-
-    // direct conversion to Mat
-    Mat hogMatrix2 = extractor.extract(image).toMat();
-    BOOST_CHECK(test::equals(hogMatrix, hogMatrix2));
-}
-
 BOOST_AUTO_TEST_CASE(hog_values) {
     vis::HogExtractor extractor;
 
-    Mat hog = extractor.extract(Mat::eye(16, 16, CV_32F)).toMat();
-    BOOST_CHECK_EQUAL(Size(4, 28), hog.size()); // NOTE size is (cols, rows)
+    arma::fmat hog = extractor.extract(cv::Mat::eye(16, 16, CV_32F));
+    BOOST_CHECK_EQUAL(28, hog.n_rows);
+    BOOST_CHECK_EQUAL(4, hog.n_cols);
 
     printmat(hog);
 
     // from Matlab
-    Mat expected = (cv::Mat_<float>(28,4) <<
+    cv::Mat temp = (cv::Mat_<float>(28,4) <<
             0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0,
@@ -100,12 +75,17 @@ BOOST_AUTO_TEST_CASE(hog_values) {
             0.047140457, 0.047140457, 0.047140457, 0.047140457,
             0.047140457, 0.047140457, 0.047140457, 0.047140457
         );
+    arma::fmat expected = vis::cv2arma<float>(temp);
 
     printmat(expected);
 
     BOOST_REQUIRE_EQUAL(expected.size(), hog.size());
-    for (int i = 0; i < expected.rows ; i++)
-        for (int j = 0; j < expected.cols; j++)
-            BOOST_REQUIRE_CLOSE_FRACTION(expected.at<float>(i, j), hog.at<float>(i,j), 1e-6);
+    BOOST_REQUIRE_EQUAL(expected.n_rows, hog.n_rows);
+    BOOST_REQUIRE_EQUAL(expected.n_cols, hog.n_cols);
+
+    for (int i = 0; i < expected.size() ; i++)
+        BOOST_REQUIRE_CLOSE_FRACTION(expected(i), hog.at(i), 1e-6);
+    //BOOST_REQUIRE(test::equals(expected, hog));
+
 }
 
