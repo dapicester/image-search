ec25ca43f8d2880fbd04bfb650a9a72e45d86a2f
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-12-17 16:51:24 +0800

    Added documentation to ImageSearch class

--- libvis/include/vis/imsearch.hpp
+++ libvis/include/vis/imsearch.hpp
@@ -21,22 +21,55 @@ class Descriptors;
 class Index;
 class Vocabulary;
 
+/// Façade class to the image search system.
 class ImageSearch {
 public:
 
+    /**
+     * Instantiate a new search system.
+     * @param category Image category.
+     * @param type Descriptors used.
+     * @param dataDir Path the data directory containing images.
+     */
     ImageSearch(const std::string& category, DescriptorsType type,
             const boost::filesystem::path& dataDir);
+
+    /// Destructor.
     ~ImageSearch();
 
+    /// Load data from saved files.
     void load();
+
+    /// Process and index images.
     void build();
+
+    /// Save data to files.
     void save() const;
 
+    /**
+     * Query by image id.
+     * @param id Query image id.
+     * @param[out] results Search results.
+     */
     void query(unsigned id, std::vector<size_t>& results) const;
+
+    /**
+     * Query by image descriptors.
+     * @param descriptors Image descriptors.
+     * @param[out] results Search results.
+     */
     void query(const arma::fvec& descriptors, std::vector<size_t>& results) const;
+
+    /**
+     * Query by image.
+     * @param image Image as read with OpenCV.
+     * @param[out] results Search results.
+     */
     void query(const cv::Mat& image, std::vector<size_t>& results) const;
 
+    /// Extract descriptors from image.
     arma::fvec extract(const cv::Mat& image) const;
+
 private:
 
     void loadIndex();
