1a9010bb57aee203dc147a6421070ec77641cca8
Paolo D'Apice <dapicester@gmail.com>
2014-02-17 21:57:56 +0800

    Fix typos

--- demo/src/recompute.cpp
+++ demo/src/recompute.cpp
@@ -19,32 +19,32 @@ Recompute::~Recompute() {}
 
 void
 Recompute::loadImagePaths(const std::string& category) {
-    std::cout << "loading images for " << category << std::cout;
+    std::cout << "loading images for " << category << std::endl;
 
     fs::path file = vis::categoryFile(dataPath, category);
     fs::path dir = dataPath / category;
     images = vis::loadNames(file, dir);
 
-    std::cout << "loaded " << images.size() << " images for " << category << std::cout;
+    std::cout << "loaded " << images.size() << " images for " << category << std::endl;
 }
 
 void
 Recompute::loadQueryImagePaths(const std::string& category) {
-    std::cout << "loading images for " << category << std::cout;
+    std::cout << "loading images for " << category << std::endl;
 
     static fs::path file = vis::categoryFile(dataPath, "test");
     static fs::path dir = dataPath / "test";
     static vector_path names = vis::loadNames(file, dir);
     queryImages = queryNames(names, str(category));
 
-    std::cout << "loaded " << queryImages.size() << " query images for " << category;
+    std::cout << "loaded " << queryImages.size() << " query images for " << category << std::endl;
 }
 
 #define PROGRESS_HANDLER(files) [&](int i) { vis::handlers::PrintFile(i, files); }
 
 void
 Recompute::computeVocabulary(const std::string& category) {
-    std::cout << "computing vocabulary for " << category;
+    std::cout << "computing vocabulary for " << category << std::endl;
 
     vector_path names = vis::subset(images, 100);
     vocabulary.reset(vis::Vocabulary::fromImageList<vis::HogExtractor>(
@@ -54,12 +54,12 @@ Recompute::computeVocabulary(const std::string& category) {
     fs::path savefile = vis::vocabularyFile(dataPath, category);
     vocabulary->save(savefile);
 
-    std::cout << "vocabulary computed" << std::cout;
+    std::cout << "vocabulary computed" << std::endl;
 }
 
 void
 Recompute::computeDescriptors(const std::string& category, const std::string& type) {
-    std::cout << "computing descriptors for " << category << " with " << type << std::cout;
+    std::cout << "computing descriptors for " << category << " with " << type << std::endl;
 
     descriptors.reset(new vis::Descriptors);
     extractDescriptors(str(category), str(type), images,
@@ -69,12 +69,12 @@ Recompute::computeDescriptors(const std::string& category, const std::string& ty
     fs::path savefile = vis::descriptorsFile(dataPath, category, type);
     descriptors->save(savefile);
 
-    std::cout << "descriptors computed " << std::cout;
+    std::cout << "descriptors computed " << std::endl;
 }
 
 void
 Recompute::computeQueries(const std::string& category, const std::string& type) {
-    std::cout << "computing query descriptors for " << category << " with " << type << std::cout;
+    std::cout << "computing query descriptors for " << category << " with " << type << std::endl;
 
     queries.reset(new vis::Descriptors);
     extractDescriptors(str(category), str(type), queryImages,
@@ -84,12 +84,12 @@ Recompute::computeQueries(const std::string& category, const std::string& type) 
     fs::path savefile = queryFile(dataPath, str(category), str(type));
     queries->save(savefile);
 
-    std::cout << "query descriptors computed " << std::cout;
+    std::cout << "query descriptors computed " << std::endl;
 }
 
 void
 Recompute::computeIndex(const std::string& category, const std::string& type) {
-    std::cout << "computing index for " << category << " with " << type << std::cout;
+    std::cout << "computing index for " << category << " with " << type << std::endl;
 
     index.reset(new vis::Index);
     index->build(category, *descriptors);
@@ -97,12 +97,12 @@ Recompute::computeIndex(const std::string& category, const std::string& type) {
     fs::path savefile = vis::indexFile(dataPath, category, type);
     index->save(savefile);
 
-    std::cout << "index computed" << std::cout;
+    std::cout << "index computed" << std::endl;
 }
 
 void
 Recompute::recompute(const std::string& category, const std::string& type) {
-    std::cout << "recomputing all for " << category << " with " << type << std::cout;
+    std::cout << "recomputing all for " << category << " with " << type << std::endl;
 
     loadImagePaths(category);
     loadQueryImagePaths(category);
@@ -112,6 +112,6 @@ Recompute::recompute(const std::string& category, const std::string& type) {
     computeQueries(category, type);
     computeIndex(category, type);
 
-    std::cout << "recomputing done" << std::cout;
+    std::cout << "recomputing done" << std::endl;
 }
 
