cd6ffdc9468c97f40a51a740ae0d3b6b6d5840c1
Paolo D'Apice <paolo@hashtag.be>
2016-03-06 16:50:03 +0800

    Fix config file path for web service

--- web/config/services.rb
+++ web/config/services.rb
@@ -5,7 +5,7 @@ ServiceManager.define_service 'image-search' do |s|
   s.port = 5858
 
   s.cwd = File.expand_path '../../../build/server', __FILE__
-  config = File.join Dir.pwd, 'server-config.yml'
+  config = File.join s.cwd, 'config.yml'
   s.start_cmd = Proc.new { "./server -p #{port} -f #{config}" }
 
   s.color = 33 # 30~37
